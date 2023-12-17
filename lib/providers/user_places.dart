c7c909a352a87a9115f026a8847998fda4570545
Zaela Rose <zaela.rose@tutanota.com>
2023-12-17 16:57:06 -0600

    init commit

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,15 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(Place place) {
+    state = [...state, place];
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+  (ref) => UserPlacesNotifier(),
+);
---
+++ lib/screens/add_place.dart
@@ -0,0 +1,65 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/providers/user_places.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new Place'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Title',
+              ),
+              controller: _titleController,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                ref
+                    .read(
+                      userPlacesProvider.notifier,
+                    )
+                    .addPlace(
+                      Place(
+                        title: _titleController.text,
+                      ),
+                    );
+                Navigator.of(context).pop();
+              },
+              icon: const Icon(Icons.add),
+              label: const Text('Add Place'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
